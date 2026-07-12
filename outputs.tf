0510965989e8af3848718e7057473f40166826e8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "automation_source_controls" {
-  description = "All automation_source_control resources"
-  value       = azurerm_automation_source_control.automation_source_controls
-}
 output "automation_source_controls_automatic_sync" {
-  description = "List of automatic_sync values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.automatic_sync]
+  description = "Map of automatic_sync values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.automatic_sync }
 }
 output "automation_source_controls_automation_account_id" {
-  description = "List of automation_account_id values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.automation_account_id]
+  description = "Map of automation_account_id values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.automation_account_id }
 }
 output "automation_source_controls_branch" {
-  description = "List of branch values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.branch]
+  description = "Map of branch values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.branch }
 }
 output "automation_source_controls_description" {
-  description = "List of description values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.description]
+  description = "Map of description values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.description }
 }
 output "automation_source_controls_folder_path" {
-  description = "List of folder_path values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.folder_path]
+  description = "Map of folder_path values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.folder_path }
 }
 output "automation_source_controls_name" {
-  description = "List of name values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.name]
+  description = "Map of name values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.name }
 }
 output "automation_source_controls_publish_runbook_enabled" {
-  description = "List of publish_runbook_enabled values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.publish_runbook_enabled]
+  description = "Map of publish_runbook_enabled values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.publish_runbook_enabled }
 }
 output "automation_source_controls_repository_url" {
-  description = "List of repository_url values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.repository_url]
+  description = "Map of repository_url values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.repository_url }
 }
 output "automation_source_controls_security" {
-  description = "List of security values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.security]
+  description = "Map of security values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.security }
 }
 output "automation_source_controls_source_control_type" {
-  description = "List of source_control_type values across all automation_source_controls"
-  value       = [for k, v in azurerm_automation_source_control.automation_source_controls : v.source_control_type]
+  description = "Map of source_control_type values across all automation_source_controls, keyed the same as var.automation_source_controls"
+  value       = { for k, v in azurerm_automation_source_control.automation_source_controls : k => v.source_control_type }
 }
 
